11defb048b7e7c6a9bc89bcf0c447033bd7136b9
graves <onesimusgravessampson1@gmail.com>
2025-09-25 00:34:49 +0000

    logout

--- oauth/urls.py
+++ oauth/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from oauth.views import EmailOTPVerificationView, LoginView, RegisterView, ResendOTPView
+from oauth.views import EmailOTPVerificationView, LoginView, LogoutView, RegisterView, ResendOTPView
 
 
 app_name = 'oauth'
@@ -9,7 +9,7 @@ urlpatterns = [
     path('auth/register/otp-verify', EmailOTPVerificationView.as_view(), name='verify-otp'),
     path('auth/resend-otp/', ResendOTPView.as_view(), name='resend-otp'),
     path('auth/login/', LoginView.as_view(), name='login'),
-    # path('auth/logout/', LogoutView.as_view(), name='logout'),
+    path('auth/logout/', LogoutView.as_view(), name='logout'),
     # path('auth/update/', UpdateUserView.as_view(), name='update-user'),
     # path('auth/me/', MeView.as_view(), name='me'),
     # path('auth/request-password-reset/', RequestPasswordReset.as_view(), name='request-password-reset'),
