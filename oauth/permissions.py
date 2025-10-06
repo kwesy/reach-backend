65273219dbe35a9ae9fab8f772e370aec7225d88
graves <onesimusgravessampson1@gmail.com>
2025-10-06 15:26:19 +0000

    added admin permissions

---
+++ oauth/permissions.py
@@ -0,0 +1,20 @@
+from rest_framework.permissions import BasePermission
+
+
+class IsAdmin(BasePermission):
+    """
+    Custom permission to only allow admin users.
+    You can modify the logic as needed.
+    """
+    def has_permission(self, request, view):
+        user = request.user
+        return user and user.is_authenticated and (user.is_staff or user.is_superuser)
+
+
+class IsAdminOrReadOnly(BasePermission):
+    def has_permission(self, request, view):
+        # Allow read for everyone
+        if request.method in ['GET', 'HEAD', 'OPTIONS']:
+            return True
+        # Only admin users can write
+        return request.user and request.user.is_authenticated and (request.user.is_staff or request.user.is_superuser)
